a048e6e09cef56542a364558d2e57e00b83a8baf
Umberto-PC\Umberto <okamirvs@gmail.com>
2019-04-15 16:00:25 +0200

    N

--- 3dGame/Assets/Project/Scripts/CameraAndCharacterMovement/MovementInput.cs
+++ 3dGame/Assets/Project/Scripts/CameraAndCharacterMovement/MovementInput.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovementInput : MonoBehaviour {
+public class MovementInput : MonoBehaviour
+{
 
     public float InputX;
     public float InputZ;
@@ -18,36 +19,85 @@ public class MovementInput : MonoBehaviour {
     public bool isGrounded;
     private float verticalVel;
     private Vector3 moveVector;
+    public GameObject particle;
 
+    private bool tapOnTheScreen = false;
+    private RaycastHit hit;
+    LayerMask layerMask = ~(1 << 10);
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         anim = this.GetComponent<Animator>();
         cam = Camera.main;
         controller = this.GetComponent<CharacterController>();
         desireRotationSpeed = 0.1f;
         allowPlayerRotation = 0.3f;
 
-}
+    }
 
-// Update is called once per frame
-void Update () {
-        InputMagnitude();
+    // Update is called once per frame
+    void Update()
+    {
 
-        // If you don't need the character grounded then get rid this part
-        isGrounded = controller.isGrounded;
-        if(isGrounded) {
-            verticalVel -= 0;
-        } else {
-            verticalVel -= 0.001f;
+        // MOBILE
+
+        // Input.touchCount count how much fingers hit the screen (max 5)
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Ended &&
+                touch.position.x < Screen.width * 5/6 && touch.position.x > Screen.width/6)
+            {
+                var ray = Camera.main.ScreenPointToRay(touch.position);
+                if (Physics.Raycast(ray, out hit, 200, layerMask))
+                {
+                    tapOnTheScreen = true;
+                    Speed = 1;
+                }
+            }
         }
 
-        moveVector = new Vector3(0, verticalVel, 0);
-        controller.Move(moveVector);
+        float distOnTheTap = Vector3.Distance(transform.position, hit.point);
+        if (tapOnTheScreen && distOnTheTap >= 0)
+        {
+            if (distOnTheTap < 0.5f)
+            {
+                Speed = 0;
+                return;
+            }
+            else if (distOnTheTap < 1f)
+            {
+                Speed = -0.1f;
+                transform.position = Vector3.MoveTowards(transform.position, hit.point, Speed * Time.deltaTime);
+            }
+
+            Vector3 heading = hit.point - transform.position;
+            float distance = heading.magnitude;
+            Vector3 desiredMoveDirection = heading / distance; // This is now the normalized direction.
+
+            // Set animator parameters
+            anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
+
+            if (blockRotationPlayer == false)
+            {
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desireRotationSpeed);
+            }
+
+            characterGrounded();
+            return;
+        }
+
+        // END MOBILE>
+
+        InputMagnitude();
+        characterGrounded();
 
     }
 
-    void PlayerMoveAndRotation() {
+    void PlayerMoveAndRotation()
+    {
         InputX = Input.GetAxis("Horizontal");
         InputZ = Input.GetAxis("Vertical");
 
@@ -63,24 +113,30 @@ void Update () {
 
         desiredMoveDirection = forward * InputZ + right * InputX;
 
-        if(blockRotationPlayer == false) {
+        if (blockRotationPlayer == false)
+        {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), desireRotationSpeed);
         }
 
         float inp = anim.GetFloat("InputMagnitude");
         float acceleration = inp - 1;   // [0, 1] it enables to accelerate, so character doesn't move faster immediately
-        if (inp > 0 && SHIFTClicked) {
+        if (inp > 0 && SHIFTClicked)
+        {
             // move forward at speed "Time.deltaTime * acceleration * 10"
             transform.Translate(new Vector3(0, 0, Time.deltaTime * acceleration * 5));
 
-        } else if (inp > 0) {
+        }
+        else if (inp > 0)
+        {
             // // move forward at speed "Time.deltaTime * acceleration * 2"
             transform.Translate(new Vector3(0, 0, Time.deltaTime * acceleration * 2));
 
         }
+
     }
 
-    void InputMagnitude() {
+    void InputMagnitude()
+    {
         // Calculate Input Vectors
         InputX = Input.GetAxis("Horizontal");
         InputZ = Input.GetAxis("Vertical");
@@ -88,7 +144,7 @@ void Update () {
         // GetKey       if hold
         // GetKeyDown   if clicked
         SHIFTClicked = Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift);
-        
+
         // Set animator parameters
         anim.SetFloat("InputZ", InputZ, 0.0f, Time.deltaTime * 2f);
         anim.SetFloat("InputX", InputX, 0.0f, Time.deltaTime * 2f);
@@ -98,24 +154,47 @@ void Update () {
         Speed = new Vector2(InputX, InputZ).sqrMagnitude;
 
         // if InputX + InputZ is equal to 2 we convert speed to 1 because we don't running if user does diagonal movements
-        if (Speed > 1) {
+        if (Speed > 1)
+        {
             Speed = 1;
         }
 
         // Physically move player
-        if (Speed > allowPlayerRotation) {
-            if (SHIFTClicked) {
+        if (Speed > allowPlayerRotation)
+        {
+            if (SHIFTClicked)
+            {
                 anim.SetFloat("InputMagnitude", Speed + 1, 0.5f, Time.deltaTime);
             }
-            else {
+            else
+            {
                 anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
             }
             PlayerMoveAndRotation();
 
-        } else if(Speed < allowPlayerRotation) {
+        }
+        else if (Speed < allowPlayerRotation)
+        {
             anim.SetFloat("InputMagnitude", Speed, 0.0f, Time.deltaTime);
 
         }
 
+    }
+
+    void characterGrounded()
+    {
+        // If you don't need the character grounded then get rid this part
+        isGrounded = controller.isGrounded;
+        if (isGrounded)
+        {
+            verticalVel -= 0;
+        }
+        else
+        {
+            verticalVel -= 0.001f;
+        }
+
+        moveVector = new Vector3(0, verticalVel, 0);
+        controller.Move(moveVector);
     }
 }
